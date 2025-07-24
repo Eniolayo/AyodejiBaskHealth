f6a2a25b55d3d4adcdf45fb2c5a86af781835e3c
Eniolayo <eniolayodeji@gmail.com>
2025-07-24 22:27:58 +0100

    refactor: consolidated CustomBottomBar component into TopProductsSection and updated SummarySection layout for improved styling and structure

--- src/components/Dashboard/SummarySection.tsx
+++ src/components/Dashboard/SummarySection.tsx
@@ -3,51 +3,62 @@ import { summaryData } from "@/data/dashboard-data";
 
 export const SummarySection = () => {
   return (
-    <div className="bg-neutral-50 p-6 rounded-lg border border-neutral-200">
-      <Typography variant="heading-04" className="text-text-primary mb-4">
-        Summary
-      </Typography>
-      <div className="space-y-4">
-        <div className="flex justify-between items-center">
-          <Typography variant="body-02" className="text-neutral-400">
+    <div className="bg-neutral-50  rounded-lg border border-neutral-200">
+      <div className="border-b px-3 py-3.5 border-neutral-200 ">
+        <Typography variant="body-01" className="text-text-primary ">
+          Summary
+        </Typography>
+      </div>
+      <div className="">
+        <div className="py-3.5 px-[15px] border-b border-neutral-200">
+          <Typography variant="body-02" className="text-neutral-400 mb-2">
             Total sales
           </Typography>
           <Typography
-            variant="body-01"
-            className="text-text-primary font-medium"
+            variant="heading-04"
+            className="text-text-primary flex items-center gap-1"
           >
-            ${summaryData.totalSales.toLocaleString()}.00 USD
+            ${summaryData.totalSales.toLocaleString()}.00
+            <Typography variant="body-01" as={"span"}>
+              USD
+            </Typography>
           </Typography>
         </div>
-        <div className="flex justify-between items-center">
-          <Typography variant="body-02" className="text-neutral-400">
+        <div className="py-3.5 px-[15px] border-b border-neutral-200">
+          <Typography variant="body-02" className="text-neutral-400 mb-2">
             Total expenses
           </Typography>
           <Typography
-            variant="body-01"
-            className="text-text-primary font-medium"
+            variant="heading-04"
+            className="text-text-primary flex items-center gap-1"
           >
-            ${summaryData.totalExpenses.toLocaleString()}.00 USD
+            ${summaryData.totalExpenses.toLocaleString()}.00
+            <Typography variant="body-01" as={"span"}>
+              USD
+            </Typography>
           </Typography>
         </div>
-        <div className="flex justify-between items-center">
-          <Typography variant="body-02" className="text-neutral-400">
+        <div className="py-3.5 px-[15px] border-b border-neutral-200">
+          <Typography variant="body-02" className="text-neutral-400 mb-2">
             Gross profit
           </Typography>
           <Typography
-            variant="body-01"
-            className="text-text-primary font-medium"
+            variant="heading-04"
+            className="text-text-primary flex items-center gap-1"
           >
-            ${summaryData.grossProfit.toLocaleString()}.00 USD
+            ${summaryData.grossProfit.toLocaleString()}.00
+            <Typography variant="body-01" as={"span"}>
+              USD
+            </Typography>
           </Typography>
         </div>
-        <div className="flex justify-between items-center">
-          <Typography variant="body-02" className="text-neutral-400">
+        <div className="py-3.5 px-[15px] ">
+          <Typography variant="body-02" className="text-neutral-400 mb-2">
             Total orders
           </Typography>
           <Typography
-            variant="body-01"
-            className="text-text-primary font-medium"
+            variant="heading-04"
+            className="text-text-primary flex items-center gap-1"
           >
             {summaryData.totalOrders.toLocaleString()}
           </Typography>
